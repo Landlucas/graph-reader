1192f7b49c4fda3ffab7f2066ef9ac9593e64738
marke-lucas-land <lucas@marke.com.br>
2019-06-12 19:07:22 -0300

    Implementado lista de adjacências e grafo visual

--- reader.js
+++ reader.js
@@ -1,103 +1,199 @@
-let numVertices = 0;
+let numVertexes = 0;
 let numEdges = 0;
 let adjacencyList = [];
 let readerForm;
 let alertsGroup;
-const sleep = m => new Promise(r => setTimeout(r, m))
+const sleep = m => new Promise( r => setTimeout( r, m ) )
 
 window.onload = function () {
-    readerForm = document.getElementById('readerForm');
-    alertsGroup = document.body.querySelector('div.alerts');
+    readerForm = document.getElementById( 'readerForm' );
+    alertsGroup = document.body.querySelector( 'div.alerts' );
 }
 
-async function processFormData(e) {
+/**
+ * Process the form data
+ * @param {Object} e event
+ */
+async function processFormData( e ) {
     e.preventDefault();
-    numVertices = document.getElementById('numVertices').value;
-    numEdges = document.getElementById('numEdges').value;
-    if (numVertices > 0 && numEdges > 0 && !readerForm.querySelector('.adjacency-group')) {
-        await createAdjacencyFields();
-    }
-    else if (!numVertices > 0 || !numEdges > 0) {
-        createAlert('Preencha o número de vertices e/ou arestas', 'danger', 'error-1');
-    }
-    else if (readerForm.querySelector('.adjacency-group')) {
-        populateAdjList();
+    numVertexes = parseInt( document.getElementById( 'numVertexes' ).value );
+    numEdges = parseInt( document.getElementById( 'numEdges' ).value );
+    if ( numVertexes > 0 && numEdges > 0 && !readerForm.querySelector( '.edge-group' ) ) {
+        if ( numEdges >= numVertexes ) {
+            createAlert( 'Número de arestas deve ser menor de que vertices', 'danger', 'error-2' );
+        } else {
+            await createEdgeFields();
+        }
+    } else if ( !numVertexes > 0 || !numEdges > 0 ) {
+        createAlert( 'Preencha o número de vertices e/ou arestas', 'danger', 'error-1' );
+    } else if ( readerForm.querySelector( '.edge-group' ) ) {
+        await populateAdjList();
+        showVisualGraph();
+        showAdjList();
     }
     return;
 }
 
+/**
+ * Populate adjacent list
+ */
 async function populateAdjList() {
-    let fields = document.querySelectorAll('.adjacency-field');
-    for (let i = 0; i < fields.length; ++i) {
-        let vertex = fields[i].value.split(',');
-        let vertexAdjList = [];
-        for (let j = 0; j < vertex.length; j++) {
-            vertexAdjList.push( parseInt(vertex[j]) );
+    for ( let j = 0; j < numVertexes; j++ ) {
+        adjacencyList.push( [] );
+    }
+    let edgeInputGroup = document.querySelectorAll( '.edge-item' );
+    for ( let i = 0; i < edgeInputGroup.length; i++ ) {
+        let vertexFrom = parseInt( edgeInputGroup[ i ].querySelector( '.edge-field-from' ).value );
+        let vertexTo = parseInt( edgeInputGroup[ i ].querySelector( '.edge-field-to' ).value );
+        if ( !adjacencyList[ vertexFrom ].includes( vertexTo ) ) {
+            adjacencyList[ vertexFrom ].push( vertexTo );
+            adjacencyList[ vertexTo ].push( vertexFrom );
         }
-        adjacencyList.push( vertexAdjList );
     }
-    console.dir(adjacencyList);
-    createAlert('Lista de adj no console log!', 'info');
+    console.dir( adjacencyList );
 }
 
-async function createAdjacencyFields() {
-
-    let fieldRow = document.createElement('div');
-    fieldRow.className = 'adjacency-group form-group row';
-    readerForm.insertBefore(fieldRow, readerForm.querySelector('button[type="submit"]'));
-
-    for (let i = 0; i < numVertices && i < 99; i++) {
-        let label = document.createElement('label');
-        label.setAttribute('for', 'vertice' + i);
-        label.className = 'col-3';
-        label.innerText = 'Vertice ' + i;
-        fieldRow.appendChild(label);
-
-        let group = document.createElement('div');
-        group.className = 'form-group col-9 mb-1';
-        fieldRow.appendChild(group);
-
-        let input = document.createElement('input');
-        input.type = 'text';
-        input.className = 'adjacency-field form-control form-control-sm';
-        input.name = 'vertice' + i;
-        group.appendChild(input);
+/**
+ * Show visual graph
+ */
+async function showVisualGraph() {
+    let nodes = [];
+    let edges = [];
+    for ( let i = 0; i < adjacencyList.length; i++ ) {
+        let vertex = adjacencyList[ i ];
+        nodes.push( {
+            id: i,
+            label: 'v' + i
+        } );
+        for ( let j = 0; j < vertex.length; j++ ) {
+            edges.push( {
+                from: i,
+                to: vertex[ j ]
+            } );
+        }
     }
-
-    readerForm.querySelector('#numVertices').disabled = true;
-    readerForm.querySelector('#numEdges').disabled = true;
-    removeAlerts('danger');
-    readerForm.querySelector('button[type="submit"]').innerHTML = 'Visualizar grafo';
+    console.log( 'nodes: ' + nodes );
+    console.log( 'edges: ' + edges );
+    let container = document.getElementById( 'visual-graph' );
+    let data = {
+        nodes: nodes,
+        edges: edges
+    };
+    let options = {
+        autoResize: true,
+        height: '100%',
+        width: '100%'
+    };
+    let network = new vis.Network( container, data, options );
 }
 
-async function createAlert(message, alertClass = 'info', id) {
-    const alert = document.createElement('div');
-    if (document.getElementById(id)) return;
+/**
+ * Show adjacency list
+ */
+async function showAdjList() {
+    let container = document.body.querySelector( '#adjacency-list' );
+    let table = document.createElement( 'table' );
+    table.className = 'table';
+    container.innerHTML = '';
+    let innerContent = '<thead><tr><th scope="col">Vértice</th><th scope="col">Adjacências</th></tr></thead><tbody>';
+    for ( let i = 0; i < adjacencyList.length; i++ ) {
+        let vertex = adjacencyList[ i ];
+        innerContent += '<tr><td>' + i + '</td><td>';
+        for ( let j = 0; j < vertex.length; j++ ) {
+            if ( j == 0 ) {
+                innerContent += vertex[ j ];
+            } else {
+                innerContent += ',' + vertex[ j ];
+            }
+        }
+        innerContent += '</td></tr>';
+    }
+    innerContent += '</tbody>';
+    table.innerHTML = innerContent;
+    container.appendChild( table );
+}
 
-    alert.setAttribute('id', id);
+/**
+ * Create edge fields
+ */
+async function createEdgeFields() {
+    let fieldRow = document.createElement( 'div' );
+    fieldRow.className = 'edge-group form-group px-lg-5';
+    readerForm.insertBefore( fieldRow, readerForm.querySelector( 'button[type="submit"]' ) );
+
+    for ( let i = 0; i < numEdges && i < 99; i++ ) {
+        let group = document.createElement( 'div' );
+        group.className = 'edge-item input-group input-group-sm mb-1';
+        fieldRow.appendChild( group );
+
+        let prepend = document.createElement( 'div' );
+        prepend.className = 'input-group-prepend';
+        prepend.innerHTML = '<div class="input-group-text">Aresta ' + i + '</div>';
+        group.appendChild( prepend );
+
+        let fromInput = document.createElement( 'input' );
+        fromInput.type = 'text';
+        fromInput.className = 'edge-field-from form-control';
+        fromInput.name = 'edge-' + i + '-from';
+        fromInput.setAttribute( 'placeholder', 'de' );
+        group.appendChild( fromInput );
+
+        let toInput = document.createElement( 'input' );
+        toInput.type = 'text';
+        toInput.className = 'edge-field-to form-control';
+        toInput.name = 'edge-' + i + '-to';
+        toInput.setAttribute( 'placeholder', 'para' );
+        group.appendChild( toInput );
+    }
+
+    readerForm.querySelector( '#numVertexes' ).disabled = true;
+    readerForm.querySelector( '#numEdges' ).disabled = true;
+    removeAlerts( 'danger' );
+    readerForm.querySelector( 'button[type="submit"]' ).innerHTML = 'Visualizar grafo';
+}
+
+/**
+ * Create an alert
+ * @param {string} message 
+ * @param {string} alertClass 
+ * @param {string} id 
+ */
+async function createAlert( message, alertClass = 'info', id ) {
+    const alert = document.createElement( 'div' );
+    if ( document.getElementById( id ) ) return;
+
+    alert.setAttribute( 'id', id );
     alert.className = 'alert alert-' + alertClass + ' alert-dismissible fade';
-    alert.setAttribute('role', 'alert');
+    alert.setAttribute( 'role', 'alert' );
     alert.innerHTML =
         message +
         '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
         '<span aria-hidden="true">&times;</span>' +
         '</button>'
-    alertsGroup.appendChild(alert);
+    alertsGroup.appendChild( alert );
 
-    await sleep(50);
-    alert.classList.add('show');
+    await sleep( 50 );
+    alert.classList.add( 'show' );
 }
 
-async function removeAlerts(alertClass = 'danger') {
-    let alerts = document.querySelectorAll('.alert-' + alertClass);
-    for (let i = 0; i < alerts.length; ++i) {
-        alerts[i].classList.remove('show');
-        removeAlert(alerts[i]);
+/**
+ * Remove alerts with given alert class
+ * @param {string} alertClass 
+ */
+async function removeAlerts( alertClass = 'danger' ) {
+    let alerts = document.querySelectorAll( '.alert-' + alertClass );
+    for ( let i = 0; i < alerts.length; ++i ) {
+        alerts[ i ].classList.remove( 'show' );
+        removeAlert( alerts[ i ] );
     }
 }
 
-async function removeAlert(alert) {
-    alert.classList.remove('show');
-    await sleep(200);
+/**
+ * 
+ * @param {Object} alert 
+ */
+async function removeAlert( alert ) {
+    alert.classList.remove( 'show' );
+    await sleep( 200 );
     alert.remove();
 }
